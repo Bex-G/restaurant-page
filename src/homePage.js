91204cddd38a2392b39bbf0cd297a531f07ea38f
Bex-G <bexgiacinto@gmail.com>
2023-09-30 20:43:45 -0700

    Create and design homePage.

---
+++ src/homePage.js
@@ -0,0 +1,52 @@
+const generateHomePage = function () {
+
+    const content = document.getElementById('content');
+    const homePage = document.createElement('div');
+    homePage.setAttribute('id', 'homePage');
+
+    const welcome = document.createElement('p');
+    welcome.setAttribute('id', 'welcome');
+    welcome.textContent = ('Welcome, weary traveler, to the');
+
+    const logoContainer = document.createElement('div');
+    logoContainer.setAttribute('id', 'logoContainer');
+    const text1 = document.createTextNode ('Black ');
+    logoContainer.appendChild(text1); 
+    const icon = document.createElement('img');
+    icon.src  = '../src/icon.png';
+    icon.setAttribute('id', 'icon');
+    logoContainer.appendChild(icon);
+    const text2 = document.createTextNode ('at Café');
+    logoContainer.appendChild(text2);
+    content.appendChild(logoContainer);
+
+    const offer = document.createElement('p');
+    offer.textContent = ('Might I offer you...');
+
+    const buttonContainer = document.createElement('div');
+    buttonContainer.setAttribute('id', 'buttonContainer');
+    
+    const drinkButton = document.createElement('button');
+    drinkButton.setAttribute('class', 'menuButton');
+    drinkButton.textContent = ('something to drink');
+
+    buttonContainer.appendChild(drinkButton);
+
+    const foodButton = document.createElement('button');
+    foodButton.setAttribute('class', 'menuButton');
+    foodButton.textContent = ('something to eat');
+    buttonContainer.appendChild(foodButton);
+
+    const citation = document.createElement('div');
+    citation.setAttribute('id', 'citation');
+    citation.textContent = ('background by Stéphane Juban Unsplash | icon by juicy_fish on flaticon');
+
+    homePage.appendChild(welcome);
+    homePage.appendChild(logoContainer);
+    homePage.appendChild(offer);
+    homePage.appendChild(buttonContainer);
+    homePage.appendChild(citation);
+    content.appendChild(homePage);
+};
+
+export default generateHomePage;
